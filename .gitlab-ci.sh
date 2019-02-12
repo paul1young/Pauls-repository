88c0b674e432dd8af7b4a9735bd3a8f55e9c6a08
LAPTOP-6Q7QMHK4\paul <paul1young4@gmail.com>
2019-02-12 16:54:56 +0000

    adding tests stuff

---
+++ .gitlab-ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xe
+composer install
+npm install
+# Copy over testing configuration.
+cp .env.gitlab .env
+npm run dev
+php artisan key:generate
+php artisan config:cache
+php artisan migrate
+php artisan db:seed
