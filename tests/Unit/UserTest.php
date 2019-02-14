8f14c9cd0a902fb169659c4768bff817705119a0
LAPTOP-6Q7QMHK4\paul <paul1young4@gmail.com>
2019-02-14 12:57:11 +0000

    testing

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+
+        $this->assertTrue(true);
+    }
+
+    public function testHasItemInBox()
+    {
+        $box = new Box(['cat', 'toy', 'torch']);
+
+        $this->assertTrue($box->has('toy'));
+        $this->assertFalse($box->has('ball'));
+    }
+}
