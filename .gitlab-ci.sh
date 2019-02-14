57b691d6fc63204e7654753cadbd951f713d8758
LAPTOP-6Q7QMHK4\paul <paul1young4@gmail.com>
2019-02-14 16:13:14 +0000

    yep

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -1,11 +1,26 @@
-#!/bin/bash
-composer install
-composer update
-npm install
-# Copy over testing configuration.
-cp .env.gitlab .env
-npm run dev
-php artisan key:generate
-php artisan config:cache
-php artisan migrate
-php artisan db:seed
+<?xml version="1.0" encoding="UTF-8"?>
+<phpunit backupGlobals="false"
+         backupStaticAttributes="false"
+         bootstrap="vendor/autoload.php"
+         colors="true"
+         convertErrorsToExceptions="true"
+         convertNoticesToExceptions="true"
+         convertWarningsToExceptions="true"
+         processIsolation="false"
+         stopOnFailure="false">
+    <testsuites>
+        <testsuite name="Application Test Suite">
+            <directory suffix=".php">/tests/</directory>
+        </testsuite>
+    </testsuites>
+
+   
+    <php>
+        <env name="APP_ENV" value="testing"/>
+        <env name="BCRYPT_ROUNDS" value="4"/>
+        <env name="CACHE_DRIVER" value="array"/>
+        <env name="MAIL_DRIVER" value="array"/>
+        <env name="QUEUE_CONNECTION" value="sync"/>
+        <env name="SESSION_DRIVER" value="array"/>
+    </php>
+</phpunit>
