48511b4fc18eaf7502ea2782178fd212e262d910
LAPTOP-6Q7QMHK4\paul <paul1young4@gmail.com>
2019-02-18 11:25:45 +0000

    addding tests hopfully works

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,19 +1,10 @@
 <?php
 
 namespace Tests;
-use App\Box;
 
-use Illuminate\Foundation\tests\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-      public function testHasItemInBox()
-    {
-        $box = new Box(['cat', 'toy', 'torch']);
-
-        $this->assertTrue($box->has('toy'));
-        $this->assertFalse($box->has('ball'));
-    }
 }
